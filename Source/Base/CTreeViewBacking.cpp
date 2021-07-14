56039712a402b0cfa86a513145626a289542cddf
Stevo <stevo@sunsetmagicwerks.com>
2021-07-13 23:54:56 -0400

    Updates to CTreeViewBacking.

--- Source/Base/CTreeViewBacking.cpp
+++ Source/Base/CTreeViewBacking.cpp
@@ -4,16 +4,36 @@
 
 #include "CTreeViewBacking.h"
 
+#include "CDictionary.h"
+#include "CUUID.h"
+
 //----------------------------------------------------------------------------------------------------------------------
-// MARK: CTreeViewBackingItem
+// MARK: CRootTreeItem
+
+class CRootTreeItem : public CTreeItem {};
+
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - CTreeViewBackingItem
 
 class CTreeViewBackingItem {
 	public:
-		CTreeViewBackingItem(const I<CTreeItem>& treeItem) : mTreeItem(treeItem) {}
-		CTreeViewBackingItem(const CTreeViewBackingItem& other) : mTreeItem(other.mTreeItem) {}
+				CTreeViewBackingItem(const I<CTreeItem>& treeItem) :
+					mViewItemID(CUUID().getBase64String()), mTreeItem(treeItem)
+					{}
+				CTreeViewBackingItem(const CTreeViewBackingItem& other) :
+					mViewItemID(other.mViewItemID), mTreeItem(other.mTreeItem),
+							mChildViewItemIDs(other.mChildViewItemIDs)
+					{}
 
-		I<CTreeItem>	mTreeItem;
+		void	addChild(const CTreeViewBackingItem& treeViewBackingItem)
+					{ mChildViewItemIDs += treeViewBackingItem.mViewItemID; }
+
+		CString				mViewItemID;
+		I<CTreeItem>		mTreeItem;
+		TNArray<CString>	mChildViewItemIDs;
 };
+
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 // MARK: - CTreeViewBackingInternals
@@ -22,12 +42,16 @@ class CTreeViewBackingInternals {
 	public:
 		CTreeViewBackingInternals() {}
 
-		TNArray<CTreeViewBackingItem>	mTopLevelTreeViewBackingItems;
+		TNDictionary<CTreeViewBackingItem>	mTreeViewBackingItemMap;
 };
 
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 // MARK: - CTreeViewBacking
+
+// MARK: Properties
+
+CString	CTreeViewBacking::mRootViewItemID(OSSTR("ROOT"));
 
 // MARK: Lifecycle methods
 
@@ -51,20 +75,77 @@ CTreeViewBacking::~CTreeViewBacking()
 void CTreeViewBacking::set(const I<CTreeItem>& rootTreeItem)
 //----------------------------------------------------------------------------------------------------------------------
 {
+	// Setup
+	mInternals->mTreeViewBackingItemMap.removeAll();
+	mInternals->mTreeViewBackingItemMap.set(mRootViewItemID, CTreeViewBackingItem(rootTreeItem));
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 void CTreeViewBacking::set(const TArray<I<CTreeItem> >& topLevelTreeItems)
 //----------------------------------------------------------------------------------------------------------------------
 {
+	// Setup
+	mInternals->mTreeViewBackingItemMap.removeAll();
+
+	CTreeViewBackingItem	rootTreeViewBackingItem(I<CTreeItem>(new CRootTreeItem()));
+	mInternals->mTreeViewBackingItemMap.set(mRootViewItemID, rootTreeViewBackingItem);
+
+	// Iterate items
+	for (TIteratorD<I<CTreeItem> > iterator = topLevelTreeItems.getIterator(); iterator.hasValue();
+			iterator.advance()) {
+		// Add
+		CTreeViewBackingItem	treeViewBackingItem(*iterator);
+		mInternals->mTreeViewBackingItemMap.set(treeViewBackingItem.mViewItemID, treeViewBackingItem);
+
+		rootTreeViewBackingItem.addChild(treeViewBackingItem);
+	}
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 void CTreeViewBacking::add(const TArray<I<CTreeItem> >& topLevelTreeItems)
 //----------------------------------------------------------------------------------------------------------------------
 {
-// Temp functionality
-for (TIteratorD<I<CTreeItem> > iterator = topLevelTreeItems.getIterator(); iterator.hasValue(); iterator.advance())
-	// Add
-	mInternals->mTopLevelTreeViewBackingItems += CTreeViewBackingItem(*iterator);
+	// Setup
+	OR<CTreeViewBackingItem>	rootTreeViewBackingItem = mInternals->mTreeViewBackingItemMap[mRootViewItemID];
+	if (!rootTreeViewBackingItem.hasReference()) {
+		// Setup root
+		mInternals->mTreeViewBackingItemMap.set(mRootViewItemID,
+				CTreeViewBackingItem(I<CTreeItem>(new CRootTreeItem())));
+		rootTreeViewBackingItem = mInternals->mTreeViewBackingItemMap[mRootViewItemID];
+	}
+
+	// Iterate items
+	for (TIteratorD<I<CTreeItem> > iterator = topLevelTreeItems.getIterator(); iterator.hasValue();
+			iterator.advance()) {
+		// Add
+		CTreeViewBackingItem	treeViewBackingItem(*iterator);
+		mInternals->mTreeViewBackingItemMap.set(treeViewBackingItem.mViewItemID, treeViewBackingItem);
+
+		rootTreeViewBackingItem->addChild(treeViewBackingItem);
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+const I<CTreeItem>& CTreeViewBacking::getTreeItem(const CString& viewItemID) const
+//----------------------------------------------------------------------------------------------------------------------
+{
+	return mInternals->mTreeViewBackingItemMap[viewItemID]->mTreeItem;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+UInt32 CTreeViewBacking::getChildCount(const CString& viewItemID) const
+//----------------------------------------------------------------------------------------------------------------------
+{
+	// Setup
+	const	OR<CTreeViewBackingItem>	treeViewBackingItem = mInternals->mTreeViewBackingItemMap[viewItemID];
+
+	return treeViewBackingItem.hasReference() ? treeViewBackingItem->mChildViewItemIDs.getCount() : 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+CString CTreeViewBacking::getChildViewItemID(const CString& viewItemID, UInt32 index) const
+//----------------------------------------------------------------------------------------------------------------------
+{
+	// Return view item id
+	return mInternals->mTreeViewBackingItemMap[viewItemID]->mChildViewItemIDs[index];
 }
--- Source/Base/CTreeViewBacking.h
+++ Source/Base/CTreeViewBacking.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "CArray.h"
+#include "CString.h"
 #include "CTreeItem.h"
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -15,17 +15,25 @@ class CTreeViewBacking {
 
 	// Methods
 	public:
-				// Lifecycle methods
-				CTreeViewBacking();
-				~CTreeViewBacking();
+								// Lifecycle methods
+								CTreeViewBacking();
+								~CTreeViewBacking();
 
-				// Instance methods
-		void	set(const I<CTreeItem>& rootTreeItem);
+								// Instance methods
+				void			set(const I<CTreeItem>& rootTreeItem);
 
-		void	set(const TArray<I<CTreeItem> >& topLevelTreeItems);
-		void	add(const TArray<I<CTreeItem> >& topLevelTreeItems);
+				void			set(const TArray<I<CTreeItem> >& topLevelTreeItems);
+				void			add(const TArray<I<CTreeItem> >& topLevelTreeItems);
+
+		const	I<CTreeItem>&	getTreeItem(const CString& viewItemID) const;
+
+				UInt32			getChildCount(const CString& viewItemID) const;
+				CString			getChildViewItemID(const CString& viewItemID, UInt32 index) const;
 
 	// Properties
+	public:
+		static	CString						mRootViewItemID;
+
 	private:
-		CTreeViewBackingInternals*	mInternals;
+				CTreeViewBackingInternals*	mInternals;
 };
