ccf894dffea8143bc9a97facf235ff06f4c1fdd4
Stevo Brock <stevo@sunsetmagicwerks.com>
2021-08-11 21:08:52 -0700

    Updated instructions for building PNG support for macOS.

--- Source/Add On - Image/macOS/buildlibpng.sh
+++ Source/Add On - Image/macOS/buildlibpng.sh
@@ -1,142 +1,65 @@
-# Builds a Libpng framework for macOS.
-#===============================================================================
-set -x
+#!/bin/bash
+#set -x
 
-: ${LIB_VERSION:=1.6.37}
+ARCHS_TO_BUILD="arm64 x86_64"
 
-: ${SDKVERSION:=`xcodebuild -showsdks | grep macosx | egrep "[[:digit:]]+\.[[:digit:]]+" -o | tail -1`}
-: ${XCODE_ROOT:=`xcode-select -print-path`}
+# Find tools
+CLANG=`xcrun --sdk macosx --find clang`
+LIPO="xcrun -sdk macosx lipo"
 
-: ${ORIGDIR:=`pwd`}
-: ${TARBALLDIR:=`pwd`}
-: ${SRCDIR:=`pwd`/src}
-: ${BUILDDIR:=`pwd`/macOS/build}
+ORIG_PATH=`pwd`
+SRC_PATH=`pwd`/../libpng-1.6.37
 
-rm -rf /tmp/build
-ln -s "`pwd`/macOS/build" /tmp/build
-: ${PREFIXDIR:=/tmp/build}
+################################################################################
+ARCH=
+CFLAGS=
+ASMFLAGS=
 
-LIB_TARBALL="$TARBALLDIR/libpng-$LIB_VERSION.tar.xz"
-LIB_SRC="$SRCDIR/libpng-${LIB_VERSION}"
+function make_lib_for_arch() {
+  # reset flags
+  ARCH="$1"
+  CFLAGS="-Wall -arch $ARCH -funwind-tables -fembed-bitcode -DPNG_ARM_NEON_OPT=0"
+  ASMFLAGS="-arch $ARCH"
 
-#===============================================================================
-ARM_DEV_CMD="xcrun --sdk macosx"
+  export CFLAGS="$CFLAGS $MIN_VER_FLAG"
+  export ASMFLAGS="$ASMFLAGS"
 
-#===============================================================================
-# Functions
-#===============================================================================
+  # Create arch folder
+  ARCH_PATH="build-$ARCH"
+  rm -rf $ARCH_PATH
+  mkdir -p $ARCH_PATH
+  cd $ARCH_PATH
 
-abort()
-{
-    echo
-    echo "Aborted: $@"
+  cat <<EOF> toolchain.cmake
+    set(CMAKE_SYSTEM_NAME Darwin)
+    set(CMAKE_SYSTEM_PROCESSOR aarch64)
+    set(CMAKE_C_COMPILER ${CLANG})
+EOF
+
+  # Run CMake
+  SDK_SYSROOT=`xcrun --sdk macosx --show-sdk-path`
+  cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_OSX_SYSROOT=${SDK_SYSROOT} "${SRC_PATH}"
+  if [ $? -ne 0 ] ; then
+    echo "Failed to configure CMake for arch $ARCH"
     exit 1
+  fi
+
+  # Run make
+  make
+  if [ $? -ne 0 ] ; then
+    echo "Failed to build for arch $ARCH"
+    exit 1
+  fi
+
+  # Back to where we started...
+  cd ..
 }
 
-doneSection()
-{
-    echo
-    echo "================================================================="
-    echo "Done"
-    echo
-}
+LIPO_ARGS=
+for arch in $ARCHS_TO_BUILD ; do
+  make_lib_for_arch $arch
+  LIPO_ARGS="$LIPO_ARGS -arch $arch build-$arch/libpng.a"
+done
 
-#===============================================================================
-
-cleanEverythingReadyToStart()
-{
-    echo Cleaning everything before we start to build...
-
-    rm -rf macOS-build
-    rm -rf "$PREFIXDIR"
-
-    doneSection
-}
-
-#===============================================================================
-
-downloadLibpng()
-{
-    if [ ! -s "$LIB_TARBALL" ]; then
-        echo "Downloading libpng ${LIB_VERSION}"
-        curl -L -o "$LIB_TARBALL" http://sourceforge.net/projects/libpng/files/libpng16/${LIB_VERSION}/libpng-${LIB_VERSION}.tar.xz
-    fi
-
-    doneSection
-}
-
-#===============================================================================
-
-unpackLibpng()
-{
-    [ -f "$LIB_TARBALL" ] || abort "Source tarball missing."
-
-    echo Unpacking libpng into $SRCDIR...
-
-    [ -d "$SRCDIR" ]    || mkdir -p "$SRCDIR"
-    [ -d "$LIB_SRC" ] || ( cd "$SRCDIR"; tar xfj "$LIB_TARBALL" )
-    [ -d "$LIB_SRC" ] && echo "    ...unpacked as $LIB_SRC"
-
-    doneSection
-}
-
-#===============================================================================
-
-buildLibpng()
-{
-    export CC=$XCODE_ROOT/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
-    export CC_BASENAME=clang
-
-    export CXX=$XCODE_ROOT/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-    export CXX_BASENAME=clang++
-
-    # avoid the `LDFLAGS` env to include the homebrew Cellar
-    export LDFLAGS=""
-    
-    cd "$LIB_SRC"
-
-    echo Building Libpng
-    export CFLAGS="-O3 -arch arm64 -arch x86_64 -isysroot $XCODE_ROOT/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDKVERSION}.sdk -Wno-error-implicit-function-declaration -fembed-bitcode"
-    export CPPFLAGS="-arch arm64 -I$XCODE_ROOT/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDKVERSION}.sdk/usr/include"
-    make distclean
-    ./configure --prefix="$PREFIXDIR/macOS-build" --disable-dependency-tracking --enable-static=yes --enable-shared=no
-    make
-    make install
-    doneSection
-}
-
-#===============================================================================
-# Execution starts here
-#===============================================================================
-
-mkdir -p "$BUILDDIR"
-
-# cleanEverythingReadyToStart #may want to comment if repeatedly running during dev
-
-echo "LIB_VERSION:       $LIB_VERSION"
-echo "LIB_SRC:           $LIB_SRC"
-echo "PREFIXDIR:         $PREFIXDIR"
-echo "SDKVERSION:        $SDKVERSION"
-echo "XCODE_ROOT:        $XCODE_ROOT"
-echo
-
-downloadLibpng
-unpackLibpng
-buildLibpng
-
-echo "Copying includes"
-cd "$ORIGDIR"
-cp -r "$PREFIXDIR/macOS-build/include" .
-
-echo "Lipo-ing library..."
-rm -rf "lib"
-mkdir "lib"
-$ARM_DEV_CMD lipo -create $PREFIXDIR/*/lib/libpng16.a -o "lib/libpng16.a" || abort "Lipo $1 failed"
-
-cd lib
-ln -s libpng16.a libpng.a
-
-echo "Completed successfully"
-cd "$ORIGDIR"
-
-#===============================================================================
+$LIPO $LIPO_ARGS -create -output libpng.a
+file libpng.a
