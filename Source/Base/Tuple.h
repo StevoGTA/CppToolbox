bfa99b420e9b7e6e068cb02d1dee27aacba1f789
Stevo <stevo@sunsetmagicwerks.com>
2025-09-12 13:04:17 -0700

    Cleanup in aisle CFolder+POSIX. Updated and added more in Tuple.

--- Source/Base/Tuple.h
+++ Source/Base/Tuple.h
@@ -10,20 +10,73 @@
 template <typename A, typename B> struct TV2 {
 	// Methods
 	public:
-			// Lifecycle methods
-			TV2(const A& a, const B& b) : mA(a), mB(b) {}
-			TV2(const TV2& other) : mA(other.mA), mB(other.mB) {}
+					// Lifecycle methods
+					TV2(const A& a, const B& b) : mA(a), mB(b) {}
+					TV2(const TV2& other) : mA(other.mA), mB(other.mB) {}
 
-			// Instamce methods
-		A&	getA() const
-				{ return mA; }
-		B&	getB() const
-				{ return mB; }
+					// Instamce methods
+		const	A&	getA() const
+						{ return mA; }
+		const	B&	getB() const
+						{ return mB; }
 
 	// Properties
 	private:
 		A	mA;
 		B	mB;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - TV3
+
+template <typename A, typename B, typename C> struct TV3 {
+	// Methods
+	public:
+					// Lifecycle methods
+					TV3(const A& a, const B& b, const C& c) : mA(a), mB(b), mC(c) {}
+					TV3(const TV3& other) : mA(other.mA), mB(other.mB), mC(other.mC) {}
+
+					// Instamce methods
+		const	A&	getA() const
+						{ return mA; }
+		const	B&	getB() const
+						{ return mB; }
+		const	C&	getC() const
+						{ return mC; }
+
+	// Properties
+	private:
+		A	mA;
+		B	mB;
+		C	mC;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - TV4
+
+template <typename A, typename B, typename C, typename D> struct TV4 {
+	// Methods
+	public:
+					// Lifecycle methods
+					TV4(const A& a, const B& b, const C& c, const D& d) : mA(a), mB(b), mC(c), mD(d) {}
+					TV4(const TV4& other) : mA(other.mA), mB(other.mB), mC(other.mC), mD(other.mD) {}
+
+					// Instamce methods
+		const	A&	getA() const
+						{ return mA; }
+		const	B&	getB() const
+						{ return mB; }
+		const	C&	getC() const
+						{ return mC; }
+		const	D&	getD() const
+						{ return mD; }
+
+	// Properties
+	private:
+		A	mA;
+		B	mB;
+		C	mC;
+		D	mD;
 };
 
 //----------------------------------------------------------------------------------------------------------------------
